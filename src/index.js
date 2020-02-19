2a54990b09aaaadf07d2253c7deb0f3560a97194
Raman N <roma.nareiko@gmail.com>
2020-02-19 23:50:51 +0300

    impl

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
 
 exports.min = function min (array) {
-  return 0;
+  if(!isValid(array))
+    return 0;
+
+  return Math.min(...array);
 }
 
 exports.max = function max (array) {
-  return 0;
+  if(!isValid(array))
+    return 0;
+
+  return Math.max(...array);
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if(!isValid(array))
+    return 0;
+
+  return array.reduce((p, c) => p + c, 0) / array.length;;
+}
+
+const isValid = function(array) {
+  return Array.isArray(array) && array.length;
 }
